cc23a9f001537cf1cf623b51fe11b0c8a215486f
Harrison Powers <harrisonpowers@gmail.com>
2019-05-31 10:24:53 -0400

    Bypass cloudflare

--- index.js
+++ index.js
@@ -7,50 +7,30 @@ let cache; // simple cache
 let lastUpdated; // cache invalidation
 const timeToUpdate = 1000 * 60 * 60 * 2; // 2 hours
 const count = 10; // how many user agents (10)
-const fallbackString = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36';
-const https = require('https');
+const fallbackString = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36';
+const url = 'https://techblog.willshouse.com/2012/01/03/most-common-user-agents/';
+const cloudscraper = require('cloudscraper');
 
 async function get() {
-  return new Promise((resolve, reject) => {
-    const options = {
-      hostname: 'techblog.willshouse.com',
-      port: 443,
-      path: '/2012/01/03/most-common-user-agents/',
-      method: 'GET'
-    };
-    const req = https.request(options, async (res) => {
-      // download with data cap
-      const maxData = 41943040; //5mb
-      let totalLength = 0;
-      let buffer = [];
-      let data;
-      try {
-        for await (const chunk of res) {
-          totalLength += chunk.length;
-          if (totalLength > maxData)
-            throw 'data cap reached';
-          buffer.push(chunk);
-        }
-        // extract JSON
-        data = buffer.join('')
-          .replace(/^[\S\s]*JSON.*\[/, '[')
-          .replace(/\][\S\s]*$/, ']');
-        // extract data
-        data = JSON.parse(data).map(d => d.useragent);
-        // data integrity
-        data = data.filter(d => d.substr(0, 13) === 'Mozilla/5.0 (');
-        data = data.slice(0,count);
-        if (data.length < count) {
-          throw 'invalid data';
-        }
-        resolve(data);
-      } catch(e) { reject(e); }
-    });
-    req.on('error', (e) => {
-      console.error(e);
-      reject(e);
-    });
-    req.end();
+  return cloudscraper.get(url).then(html => {
+    let data;
+    // extract JSON
+    data = html
+      .replace(/^[\S\s]*JSON.*\[/, '[')
+      .replace(/\][\S\s]*$/, ']');
+    // extract data
+    data = JSON.parse(data).map(d => d.useragent);
+    // data integrity
+    data = data.filter(d => d.substr(0, 13) === 'Mozilla/5.0 (');
+    data = data.slice(0,count);
+    if (data.length < count) {
+      throw 'invalid data';
+    }
+    return data;
+  })
+  .catch(err => {
+    console.log('sourcing user-agents failed: using fallback');
+    return fallbackString;
   });
 }
 
