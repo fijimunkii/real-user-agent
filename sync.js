c596d840656b9d20c0e21621d895d4ce374155aa
Harrison Powers <harrisonpowers@gmail.com>
2019-06-05 10:47:38 -0400

    Sync now updates local file

--- sync.js
+++ sync.js
@@ -1,12 +1,9 @@
 const count = 10; // how many user agents (10)
 const url = 'https://techblog.willshouse.com/2012/01/03/most-common-user-agents/';
-const GIST_ID = '952acac988f2d25bef7e0284bc63c406';
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-if (!GITHUB_TOKEN) {
-  throw new Error('Missing env GITHUB_TOKEN');
-}
-const https = require('https');
 const cloudscraper = require('cloudscraper');
+const writeFile = require('util').promisify(require('fs').writeFile);
+const readFile = require('util').promisify(require('fs').readFile);
+const file = require('path').join(__dirname,'ua.json');
 
 async function get() {
   return cloudscraper.get(url).then(html => {
@@ -28,43 +25,20 @@ async function get() {
 }
 
 async function set(data) {
-  return new Promise((resolve, reject) => {
-    const postData = JSON.stringify({files:{"ua.json":{content:JSON.stringify(data,null,2)}}});
-    const options = {
-      hostname: 'api.github.com',
-      port: 443,
-      path: `/gists/${GIST_ID}`,
-      method: 'PATCH',
-      headers: {
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: `token ${GITHUB_TOKEN}`,
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(postData),
-        'User-Agent': 'fijimunkii'
-      }
-    };
-    const req = https.request(options, async (res) => {
-      let resBody = [];
-      res.on('data', d => resBody.push(d));
-      res.on('end', () => {
-        if (res.statusCode !== 200) {
-          reject(Buffer.from(resBody).toString('utf8'));
-        }
-        resolve();
-      });
-    });
-    req.on('error', reject);
-    req.write(postData);
-    req.end();
-  });
+  data = JSON.stringify(data,null,2);
+  const existingData = (await readFile(file)).toString();
+  if (existingData === data) {
+    console.log('No changes');
+    return;
+  }
+  await writeFile(file,data);
+  console.log('New user agents synced');
 }
 
 async function sync() {
   try {
     const data = await get();
     await set(data);
-    const gist = `https://gist.githubusercontent.com/fijimunkii/${GIST_ID}/raw/ua.json`;
-    console.log(`User agents synced: ${url} -> ${gist}`);
   } catch(err) {
     console.log(err);
   }
